45458d3e699420f6bd0d68350c0ef5d8bb41c097
alex-hlh <hlh3727138@gmail.com>
2023-03-24 21:41:29 +0800

    Initial commit

---
+++ src/test/java/com/hua/flow/Axe/AxeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hua.flow.Axe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
